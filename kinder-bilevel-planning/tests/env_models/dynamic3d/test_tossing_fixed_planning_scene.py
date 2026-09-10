4d2f34da6fd36783f16b19522faeffede46dff8b
merlerm <matteo.merler@gmail.com>
2026-09-10 15:03:57 +0200

    Use the typed Gym interface when closing the Tossing test environment

--- kinder-bilevel-planning/tests/env_models/dynamic3d/test_tossing_fixed_planning_scene.py
+++ kinder-bilevel-planning/tests/env_models/dynamic3d/test_tossing_fixed_planning_scene.py
@@ -1,8 +1,11 @@
 """The tossing planner must see the same fixed obstacles as the simulator."""
 
+from typing import cast
+
 import kinder
 import numpy as np
 import pybullet as p
+from gymnasium import Env
 from kinder.envs.dynamic3d.envs import ObjectCentricTidyBot3DEnv, TidyBot3DEnv
 from kinder.envs.dynamic3d.object_types import (
     MujocoFixtureObjectType,
@@ -99,4 +102,4 @@ def test_fixed_tossing_obstacles_match_both_planning_scenes(monkeypatch) -> None
     finally:
         for planner in captured:
             planner.close()
-        env.close()
+        cast(Env, env).close()
